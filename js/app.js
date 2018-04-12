ebb9741c679a342fd0f136748d48a6c6186858dd
halverisa <halverisa@gmail.com>
2018-04-12 17:35:56 -0500

    efecto_teclado
    efecto a las teclas cuando oprime

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+
+/*metodos para efecto del teclado*/
+document.getElementById("on").addEventListener("mousedown", function(){
+document.getElementById("on").setAttribute("style","transform:scale(0.95,0.95)")
+})
+document.getElementById("on").addEventListener("mouseout", function(){
+document.getElementById("on").setAttribute("style","transform:scale(1,1)")
+})
+document.getElementById("sign").addEventListener("mousedown", function(){
+document.getElementById("sign").setAttribute("style","transform:scale(0.95,0.95)")
+})
+document.getElementById("sign").addEventListener("mouseout", function(){
+document.getElementById("sign").setAttribute("style","transform:scale(1,1)")
+})
